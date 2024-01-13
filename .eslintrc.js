2d099bbef55163997ea394f9941b79c4388fa019
KarimPortfolio <mohamedkarmballa19@gmail.com>
2024-01-13 17:57:22 +0100

    initial commit for todo-app mobile app

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "@react-native-community"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        semi: ["error", "never"],
+        'comma-dangle': [2, 'never'],
+        'react/jsx-filename-extension':[1, {extensions: ['.js', '.jsx']}],
+        'no-use-before-define': [
+            'error',
+            { functions:true, classes:true, variables:false }
+        ]
+    }
+}
